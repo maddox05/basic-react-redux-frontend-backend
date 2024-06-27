e241b6a5b4e3d8d30575752bc5751a1851417d19
maddox05 <maddoxschmidlkofer@gmail.com>
2024-06-27 13:20:06 -0400

    animal app done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+
+import React from 'react';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow.js';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
+  function renderAnimals(animalsArr) {
+    animalsArr = animalsArr.map((animal, index) => {
+      return <AnimalShow type={animal} key={index} />;
+    });
+    return animalsArr;
+  }
+
+  function handleClick() {
+    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>Animals Shown: {count}</div>
+      <div className='animal-list'>{renderAnimals(animals)}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'; // react dom shows the react component.
+import App from './App.js';
+import 'semantic-ui-css/semantic.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Main() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Main />, document.getElementById('root'));
