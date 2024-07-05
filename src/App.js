6e577a6a8b79e245348e46aeea49cd56e08189ac
maddox05 <maddoxschmidlkofer@gmail.com>
2024-07-05 16:00:53 -0400

    new app incoming

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import React, { useState } from 'react';
-import { Segment } from 'semantic-ui-react';
-
-import { getImagesByQuery } from './axios_api.js';
-import SearchBar from './components/SearchBar.js';
-import ImageList from './components/ImageList.js';
 
 export default function App() {
-  const [apiList, setApiList] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-
-  async function handleSearchBarSubmit(value) {
-    setLoading(true);
-    setApiList([...apiList, ...(await getImagesByQuery(value))]);
-    setLoading(false);
-  }
-  return (
-    <Segment>
-      <SearchBar onSubmit={handleSearchBarSubmit} />
-      <Segment basic loading={isLoading}>
-        <ImageList objList={apiList}></ImageList>
-      </Segment>
-    </Segment>
-  );
+  <></>;
 }
--- src/components/ImageShow.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Image } from 'semantic-ui-react';
-
-export default function ImageShow({ desc, link }) {
-  return <Image alt={desc} src={link} width={100} />;
-}
