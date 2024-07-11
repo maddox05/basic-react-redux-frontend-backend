12d7c737e11767e6c5f7dc55d840642b0d8d78d2
maddox05 <maddoxschmidlkofer@gmail.com>
2024-07-11 17:10:58 -0400

    wwwww added editing books

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
-import { Item, Button, Input } from 'semantic-ui-react';
+import { Item, Button, Input, Form } from 'semantic-ui-react';
 
 export default function BookShow({ id, title, onEdit, onDel }) {
   const [inputVal, setInputVal] = useState('');
+  const [editOpen, setEditOpen] = useState(false);
   return (
     <Item>
       <Item.Image size='tiny' src='https://duckmath.org/assets/img/scaled_goose_pixel.png' />
       <Item.Content>
         <Item.Header>{title}</Item.Header>
-        <Button icon='edit' />
         <Button
-          icon='check'
+          icon='edit'
           onClick={() => {
-            onEdit(id, inputVal);
+            setEditOpen(!editOpen);
           }}
         />
-        <Input
-          value={inputVal}
-          onChange={(event, data) => {
-            setInputVal(event.target.value);
-          }}
-        />
+        {editOpen ? (
+          <Form
+            onSubmit={(event) => {
+              event.preventDefault();
+              onEdit(id, inputVal);
+              setInputVal('');
+              setEditOpen(!editOpen);
+            }}
+          >
+            <Button icon='check' />
+            <Input
+              value={inputVal}
+              onChange={(event, data) => {
+                setInputVal(event.target.value);
+              }}
+            />
+          </Form>
+        ) : (
+          <></>
+        )}
+
         <Button
           icon='delete'
           onClick={() => {
